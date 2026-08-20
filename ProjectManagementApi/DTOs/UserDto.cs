10c77cc9e1886771b3e224f65a79b682a780754a
Nasrullah Asghar <153411978+nasrullahasghar@users.noreply.github.com>
2026-08-20 11:32:34 +0500

    Delete ProjectManagementApi directory

--- ProjectManagementApi/DTOs/UserDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectManagementApi.DTOs;
-
-public class UserDto
-{
-    public int Id { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
-}
